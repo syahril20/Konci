2d67165fdfd7d006937bd5b639b77bd5fb927e4d
Syahril20 <rodyulo20@gmail.com>
2020-06-30 23:29:37 +0700

    bebas

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 class SiswaController extends Controller
 {
+    public function index(){
     $Siswa = DB::table('Siswa')->get();
         return view('index', ['Siswa' => $Siswa]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/pegawai','MahasiswaController@index');
+Route::get('/Siswa','SiswaController@index');
