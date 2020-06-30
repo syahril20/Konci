ab9ca90bd309549921daad7f6bb7f6fc73144e73
Syahril20 <rodyulo20@gmail.com>
2020-06-30 23:49:17 +0700

    bebas

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('siswa', function () {
-    return view('index');
+    return view('welcome');
 });
 
-Route::get('Siswa','SiswaController@index');
+Route::get('/',function () {
+    return view('index');
+});
