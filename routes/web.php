5c17720c6c30408fb25e703592b14f15339062a7
Syahril20 <rodyulo20@gmail.com>
2020-07-01 00:22:57 +0700

    bebas

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('ytf', function () {
     return view('welcome');
 });
-
-Route::get('/s',function () {
-    return ('Memek');
-});
-
-Route::get('/Siswa', 'SiswaController@index');
